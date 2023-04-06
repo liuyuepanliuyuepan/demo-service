f85066d1a05c493a050338cf26a4bdb262ec6e2c
liuyuepan <liuyuepan@mingyuebohong.com>
2023-04-06 14:07:46 +0800

    新增xxl-job相关配置

--- src/main/java/cn/klmb/crm/CrmApplication.java
+++ src/main/java/cn/klmb/crm/CrmApplication.java
@@ -2,10 +2,13 @@ package cn.klmb.crm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 /**
  * 项目的启动类
  */
 @SuppressWarnings("SpringComponentScan") // 忽略 IDEA 无法识别 ${crm.info.base-package}
+@EnableScheduling
 @SpringBootApplication(scanBasePackages = {"${crm.info.base-package}"})
 public class CrmApplication {
 
