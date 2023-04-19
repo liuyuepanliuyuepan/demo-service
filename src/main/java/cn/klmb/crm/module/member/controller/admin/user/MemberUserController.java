8f6c00d4765d33ee10afd9f1b4864ce0f228b5bb
liuyuepan <liuyuepan@mingyuebohong.com>
2023-04-19 11:51:54 +0800

    测试

--- src/main/java/cn/klmb/crm/module/member/controller/admin/user/MemberUserController.java
+++ src/main/java/cn/klmb/crm/module/member/controller/admin/user/MemberUserController.java
@@ -390,7 +390,7 @@ public class MemberUserController {
             @ApiImplicitParam(name = "lastBizId", value = "业务id", paramType = "query", dataTypeClass = String.class),
             @ApiImplicitParam(name = "pageSize", value = "每页数量，默认10", paramType = "query", dataTypeClass = Integer.class),
             @ApiImplicitParam(name = "asc", value = "是否为正序", paramType = "query", dataTypeClass = Boolean.class)})
-    @PreAuthorize("@ss.hasPermission('system:user:query')")
+    @PreAuthorize("@ss.hasPermission('member:user:query')")
     public CommonResult<KlmbScrollPage<MemberUserRespVO>> pageScrollPool(
             @Valid MemberUserScrollPageReqVO reqVO) {
         reqVO.setPoolId("0");
