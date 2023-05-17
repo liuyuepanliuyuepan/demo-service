372c6fbb398a98556db92ff0420ca2180578784d
liuyuepan <liuyuepan@mingyuebohong.com>
2023-05-17 16:40:17 +0800

    优化合同详情

--- src/main/java/cn/klmb/crm/module/contract/controller/admin/product/vo/ContractProductRespVO.java
+++ src/main/java/cn/klmb/crm/module/contract/controller/admin/product/vo/ContractProductRespVO.java
@@ -1,8 +1,11 @@
 package cn.klmb.crm.module.contract.controller.admin.product.vo;
 
-import lombok.*;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import java.time.LocalDateTime;
-import io.swagger.annotations.*;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @ApiModel("管理后台 - 合同产品关系 Response VO")
 @Data
@@ -15,5 +18,11 @@ public class ContractProductRespVO extends ContractProductBaseVO {
 
     @ApiModelProperty(value = "创建时间", required = true)
     private LocalDateTime createTime;
+
+    @ApiModelProperty(value = "产品名称")
+    private String productName;
+
+    @ApiModelProperty(value = "产品类别")
+    private String categoryName;
 
 }
